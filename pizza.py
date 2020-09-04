15e7b69d2ca42fe7f61d451ab64d0ad7079631e3
Gio1845 <victor-reyesr@hotmail.com>
2020-09-04 12:08:14 -0600

    segundocommit

--- pizza.py
+++ pizza.py
@@ -2,7 +2,7 @@
 pi =3.14
 radio = int(input("Ingresa el valor del radio que tendra su pizza: "))
 area = (radio ** 2) * pi
-precio = 220
+precio = 10
 total=   area / precio
  
 print("el costo es de " ,total,"Pesos")
